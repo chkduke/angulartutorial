f0d030f0dce2dbf0d3ef214e430f2d02faf67181
Carlos Duque <carduest@gmail.com>
2013-04-09 17:54:46 +0200

    Initial files added

---
+++ AngularTut/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace AngularTut.Migrations
+{
+    using AngularTut.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AngularTut.Models.TodoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(AngularTut.Models.TodoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            var r = new Random();
+            var items = Enumerable.Range(1, 50).Select(o => new Todo
+            {
+                DueDate = new DateTime(2012, r.Next(1, 12), r.Next(1, 28)),
+                Priority = (byte)r.Next(10),
+                Text = o.ToString()
+            }).ToArray();
+
+           
+            context.Todoes.AddOrUpdate(item => new { item.Text }, items);
+        }
+    }
+}
